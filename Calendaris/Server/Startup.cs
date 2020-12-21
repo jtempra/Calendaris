6536beba40f055a0cf2e1de08835f767caa65889
Josep Temprà <jtempra@anoiaint.com>
2020-12-21 18:19:06 +0100

    commit2

--- Calendaris/Server/Startup.cs
+++ Calendaris/Server/Startup.cs
@@ -1,7 +1,9 @@
+using Calendaris.Server.Contextes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +24,7 @@ namespace Calendaris.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<CalendarisDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CalendarisDB"))); ;
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
